4940bf93e5ab9ee7abe649c96e374a64a515497c
Noelby13 <hnoelreyes25@gmail.com>
2023-04-10 10:34:16 -0600

    se agrega frmListaTrabajo

---
+++ Formularios/FrmListaTrabajo.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmListaTrabajo
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Orden = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.HoraInicio = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Estado = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PanelLeyendaFactura = New System.Windows.Forms.Panel()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelLeyendaFactura.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(635, 685)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Controls.Add(Me.PanelLeyendaFactura)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(627, 659)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Ordenes"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Orden, Me.HoraInicio, Me.Nombre, Me.Estado})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 54)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(621, 491)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Orden
+        '
+        Me.Orden.HeaderText = "Orden"
+        Me.Orden.Name = "Orden"
+        '
+        'HoraInicio
+        '
+        Me.HoraInicio.HeaderText = "Hora Inicio"
+        Me.HoraInicio.Name = "HoraInicio"
+        '
+        'Nombre
+        '
+        Me.Nombre.HeaderText = "Nombre"
+        Me.Nombre.Name = "Nombre"
+        '
+        'Estado
+        '
+        Me.Estado.HeaderText = "STAT"
+        Me.Estado.Name = "Estado"
+        '
+        'PanelLeyendaFactura
+        '
+        Me.PanelLeyendaFactura.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(228, Byte), Integer))
+        Me.PanelLeyendaFactura.Controls.Add(Me.BtnSalir)
+        Me.PanelLeyendaFactura.Controls.Add(Me.Label1)
+        Me.PanelLeyendaFactura.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelLeyendaFactura.Location = New System.Drawing.Point(3, 3)
+        Me.PanelLeyendaFactura.Name = "PanelLeyendaFactura"
+        Me.PanelLeyendaFactura.Size = New System.Drawing.Size(621, 51)
+        Me.PanelLeyendaFactura.TabIndex = 1
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.BtnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSalir.ForeColor = System.Drawing.Color.White
+        Me.BtnSalir.Location = New System.Drawing.Point(541, 7)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(68, 32)
+        Me.BtnSalir.TabIndex = 1
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(120, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(243, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "El tablero se actualizará en : "
+        '
+        'FrmListaTrabajo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 685)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "FrmListaTrabajo"
+        Me.Text = "FrmListaTrabajo"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelLeyendaFactura.ResumeLayout(False)
+        Me.PanelLeyendaFactura.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Orden As DataGridViewTextBoxColumn
+    Friend WithEvents HoraInicio As DataGridViewTextBoxColumn
+    Friend WithEvents Nombre As DataGridViewTextBoxColumn
+    Friend WithEvents Estado As DataGridViewTextBoxColumn
+    Friend WithEvents PanelLeyendaFactura As Panel
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents Label1 As Label
+End Class
