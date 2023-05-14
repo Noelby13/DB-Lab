978d69cc8a6d3b1dfe1ac8837fe3dffc5fcea7c2
Noelby13 <hnoelreyes25@gmail.com>
2023-05-14 13:01:02 -0600

    se agrega el frmCerrarTurno. Tambien se añade funciones a DArqueo y DUsuario

---
+++ Formularios/FrmCerrarTurno.vb
@@ -0,0 +1,19 @@
+﻿Public Class FrmCerrarTurno
+    Friend Property cierreExitoso As Boolean = False
+    Friend Property usuario As Usuario
+
+    Private Sub FrmCerrarTurno_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dArqueo As New DArqueo
+        Dim turno = dArqueo.obtenerTurno()
+        LblFecha.Text = DateTime.Now.ToString("dd/MM/yyyy")
+        'LblUsuario.Text = usuario.Username
+        TxtMontoInicial.Text = turno.MontoInicial.ToString()
+
+    End Sub
+
+    Private Function calcularMontoFinal() As Decimal
+
+
+    End Function
+
+End Class
